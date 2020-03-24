ea3d58ffec812039b100ecb5a96633f20782915f
Myra Fah <FahMyra@JohnDeere.com>
2020-03-24 17:53:37 -0500

    formatting event component

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,17 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Items from "./Items";
 
-
 // {/* need to move items to event details still */}
 const Event = props => {
+
+  let date =  new Date(props.event.date)
+  var monthNames = [
+    "Jan", "Feb", "Mar",
+    "Apr", "May", "Jun", "Jul",
+    "Aug", "Sep", "Oct",
+    "Nov", "Dec"
+    ];
+    let d = date.getDate();
+    let m = monthNames[date.getMonth()];
+
+  date = `${date.getFullYear()}-${m}-${date.getDate()} `
+
   return (
     <div>
-        <Link to={`/event/${props.event._id}`} className="event-row">
-        {props.event.date}: {props.event.description} {props.event.location}
-        </Link>
-      <button id={props.event._id} onClick={props.handleDelete}>
+
+      <input
+        onSubmit={props.handleSubmit}
+        type='submit'
+        className='delete-button'
+        value='-'
+      />
+
+      <Link to={`/event/${props.event._id}`} className='event-row'>
+        {date}: {props.event.description} {props.event.location}
+      </Link>
+      {/* <button id={props.event._id} onClick={props.handleDelete}>
       Delete
-    </button>
+    </button> */}
       {/* <Items items={props.items} /> */}
     </div>
   );
